49c6f64454de4dd71722113f0fe77d91eaa95eb2
Shahzar Ansari <shahzar.ansari94@gmail.com>
2021-12-27 22:59:08 +0530

    removed unused variables

--- sleep_when_you_die.py
+++ sleep_when_you_die.py
@@ -1,17 +1,15 @@
 import pyautogui
-from random import randint
 import win32api
 
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
-current_pos_x, current_pos_y= pyautogui.position()
-size_x, size_y= pyautogui.size()
+mins= 5
 
 while(True):
 	idle= int((win32api.GetTickCount() - win32api.GetLastInputInfo()) / 1000.0)
 	print(idle)
-	if idle!=0 and idle%300 == 0:    # if idle for 5 mins
+	if idle!=0 and idle%(mins*60) == 0:    # if idle for 5 mins
 		pyautogui.press('capslock')
 		pyautogui.press('capslock')
 
